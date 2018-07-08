49ca49edd21c020680a95200057a79c460f2b828
alimcmaster1 <alimcmaster1@gmail.com>
2018-07-08 16:52:09 +0100

    Initial Hello World example
    Ubuntu pip bug...
    
    Ubuntu pip bug...
    
    Use Port env var
    
    Use Port env var
    
    set host

---
+++ app/app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Testing 1,2,3"
+
+
+if __name__ == '__main__':
+    # Heroku defines the port we must use in the "PORT" env variable
+    port = int(os.environ.get("PORT", 8000))
+    print("Port to use {}".format(port))
+    app.run(debug=True, host='0.0.0.0', port=port)
